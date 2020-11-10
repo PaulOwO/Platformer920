87e3d098ff539e77bdc7bb90170a23a8252210df
PaulOwO <paul.breuza@gmail.com>
2020-11-10 15:50:26 +0100

    test

--- include/game.h
+++ include/game.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "player_character.h"
 #include "SFML/Graphics/RenderWindow.hpp"
 #include "SFML/Graphics/Texture.hpp"
 #include "box2d/b2_world.h"
@@ -12,5 +13,8 @@ public:
 private:
     sf::RenderWindow window_;
     sf::Texture wall_;
+    sf::Sprite sprite_;
     b2World world_{b2Vec2(0.0f, 9.81f)};
+
+    PlayerCharacter player_;
 };
--- src/game.cpp
+++ src/game.cpp
@@ -2,33 +2,61 @@
 
 #include "SFML/Window/Event.hpp"
 #include "SFML/Graphics/Sprite.hpp"
+#include <iostream>
 
 Game::Game() = default;
 
 void Game::Init()
 {
     window_.create(sf::VideoMode(1280, 720), "SAE Platformer");
-    wall_.loadFromFile("data/sprites/wall.jpg");
+    window_.setVerticalSyncEnabled(true);
+    if (!wall_.loadFromFile("data/sprites/wall.jpg"))
+    {
+        std::cerr << "Texture Eror\n";
+    }
+    sprite_.setTexture(wall_);
+    sprite_.setOrigin(wall_.getSize().x / 2.0f , wall_.getSize().y / 2.0f);
+
+    player_.Init();
 }
 
 void Game::Loop()
 {
+    sf::Clock gameClock_;
     while(window_.isOpen())
     {
+        auto dt = gameClock_.restart();
         sf::Event event;
         while (window_.pollEvent(event))
         {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
+            {
                 window_.close();
+                return;
+            }
+            if (event.type == sf::Event::Resized)
+            {
+                auto view = window_.getView();
+                view.setSize(event.size.width, event.size.height);
+                window_.setView(view);
+            }
         }
+
+        player_.Update(dt.asSeconds());
+        //Window view follows the player character
+        auto view = window_.getView();
+        view.setCenter(player_.GetPosition());
+        window_.setView(view);
+
+
         window_.clear();
-        sf::Sprite sprite;
-        sprite.setTexture(wall_);
-        sprite.setOrigin(wall_.getSize().x / 2.0f, wall_.getSize().y / 2.0f);
-        const auto center = window_.getSize();
-        sprite.setPosition(center.x/2.0f, center.y/2.0f);
-        window_.draw(sprite);
+        const auto windowsSize = window_.getSize();
+        sprite_.setPosition(windowsSize.x / 2.0f, windowsSize.y / 2.0f);
+        window_.draw(sprite_);
+
+        player_.Render(window_);
+
         window_.display();
     }
 }
